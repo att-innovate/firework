aa7fa2cbaed3aaacc73e79442dfe1910d3cb7e9a
Mario Admon <mladmon589@gmail.com>
2017-01-27 16:39:55 -0800

    Added HDL skeleton file from Qsys Component Editor tool (need to merge w/ top-level entity)

--- varint_encoder_top.v
+++ varint_encoder_top.v
@@ -17,3 +17,29 @@ module varint_encoder_top (
 	);
 
 endmodule
+
+
+// new_component.v
+
+// This file was auto-generated as a prototype implementation of a module
+// created in component editor.  It ties off all outputs to ground and
+// ignores all inputs.  It needs to be edited to make it do something
+// useful.
+// 
+// This file will not be automatically regenerated.  You should check it in
+// to your version control system if you want to keep it.
+
+`timescale 1 ps / 1 ps
+module new_component (
+        input  wire [31:0] asi_in0_data,  // asi_in0.data
+        output wire        asi_in0_ready, //        .ready
+        input  wire        asi_in0_valid, //        .valid
+        input  wire        clock_clk,     //   clock.clk
+        input  wire        reset_reset    //   reset.reset
+    );
+
+    // TODO: Auto-generated HDL template
+
+    assign asi_in0_ready = 1'b0;
+
+endmodule
