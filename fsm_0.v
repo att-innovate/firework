fdd3b0c3c2b086e25dba223acee718aba068b581
Mario Admon <mladmon589@gmail.com>
2017-03-15 16:47:00 -0700

    implemented fsm_0 next state logic

--- fsm_0.v
+++ fsm_0.v
@@ -1,7 +1,7 @@
 // fsm_0.v
 
 module fsm_0 (
-		// global signals (clock, reset)
+		// global signals
 		input  wire        clk,
 		input  wire        reset,
 
@@ -44,35 +44,176 @@ module fsm_0 (
 		output reg  [9:0]  index
 	);
 
-		// state encoding (one-hot encoding)
+		// internal registers
+		reg [3:0]  awid;
+		reg [31:0] awaddr;
+		reg [7:0]  awlen;
+		reg [2:0]  awsize;
+		reg [1:0]  awburst;
+	
+		// state definitions (one-hot encoding)
 		parameter INIT        = 16'h0001,
 		          AW_READY    = 16'h0002,
 		          W_READY_VN  = 16'h0004,
 		          W_READY_VL  = 16'h0008,
 		          W_READY_RN  = 16'h0010,
 		          W_READY_RL  = 16'h0020,
-		          FIFO_FULL_V = 16'h0040,
-		          FIFO_FULL_R = 16'h0080,
-		          B_READY_0   = 16'h0100,
-		          B_READY_1   = 16'h0200,
-		          B_READY_2   = 16'h0400,
-		          B_READY_3   = 16'h0800,
+		          VF_FULL     = 16'h0040,
+		          RF_FULL     = 16'h0080,
+		          B_READY_VN  = 16'h0100,
+		          B_READY_VL  = 16'h0200,
+		          B_READY_RN  = 16'h0400,
+		          B_READY_RL  = 16'h0800,
 		          MASTER_WAIT = 16'h1000;
-					 
-		// internal registers
+
+		// state memory logic
 		reg [15:0] state;
-		reg [15:0] next_state;		
-
-		reg [3:0]  awid;
-		reg [31:0] awaddr;
-		reg [7:0]  awlen;
-		reg [2:0]  awsize;
-		reg [1:0]  awburst;
-
-		// next state logic
+		reg [15:0] next_state;
 		
-		// state memory
-		
-		// output logic
+		always @ (posedge clk)
+		begin
+			if (reset)
+				state <= INIT;
+			else
+				state <= next_state;
+		end
+
+		// next state, output logic
+		always @*
+		begin
+			case (state)
+				INIT:		
+					begin
+						next_state = AW_READY;
+					end
+
+				AW_READY:
+					begin
+						if (~axs_s0_awvalid)
+							next_state = AW_READY;
+						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'h0x
+										&& varint_in_fifo_full)
+							next_state = VF_FULL;
+						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'h00 
+										&& ~varint_in_fifo_full)
+							next_state = W_READY_VN;
+						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'h01
+										&& ~varint_in_fifo_full)
+							next_state = W_READY_VL;
+						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'hFx
+										&& raw_data_in_fifo_full)
+							next_state = RF_FULL;
+						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'hF0
+										&& ~raw_data_in_fifo_full)
+							next_state = W_READY_RN;
+						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'hF1
+										&& ~raw_data_in_fifo_full)
+							next_state = W_READY_RL;
+						else 
+							next_state = INIT;
+					end
+
+				W_READY_VN:
+					begin
+						if (~axs_s0_wvalid)
+							next_state = W_READY_VN;
+						else
+							next_state = B_READY_VN;
+					end
+				
+				W_READY_VL:
+					begin
+						if (~axs_s0_wvalid)
+							next_state = W_READY_VL;
+						else
+							next_state = B_READY_VL;
+					end
+				
+				W_READY_RN:
+					begin
+						if (~axs_s0_wvalid)
+							next_state = W_READY_RN;
+						else
+							next_state = B_READY_RN;
+					end
+				
+				W_READY_RL:
+					begin
+						if (~axs_s0_wvalid)
+							next_state = W_READY_RL;
+						else
+							next_state = B_READY_RL;
+					end
+				
+				VF_FULL:
+					begin
+						if (varint_in_fifo_full)
+							next_state = VF_FULL;
+						else if (~varint_in_fifo_full && awaddr[7:0] == 8'h00)
+							next_state = W_READY_VN;
+						else if (~varint_in_fifo_full && awaddr[7:0] == 8'h01)
+							next_state = W_READY_VL;
+						else
+							next_state = INIT;
+					end
+				
+				RF_FULL:
+					begin
+						if (raw_data_in_fifo_full)
+							next_state = RF_FULL;
+						else if (~raw_data_in_fifo_full && awaddr[7:0] == 8'hF0)
+							next_state = W_READY_RN;
+						else if (~raw_data_in_fifo_full && awaddr[7:0] == 8'hF1)
+							next_state = W_READY_RL;
+						else
+							next_state = INIT;
+					end
+				
+				B_READY_VN:
+					begin
+						if (~axs_s0_bready)
+							next_state = MASTER_WAIT;
+						else
+							next_state = AW_READY;
+					end
+				
+				B_READY_VL:
+					begin
+						if (~axs_s0_bready)
+							next_state = MASTER_WAIT;
+						else
+							next_state = AW_READY;
+					end
+
+				B_READY_RN:
+					begin
+						if (~axs_s0_bready)
+							next_state = MASTER_WAIT;
+						else
+							next_state = AW_READY;
+					end
+				
+				B_READY_RL:
+					begin
+						if (~axs_s0_bready)
+							next_state = MASTER_WAIT;
+						else
+							next_state = AW_READY;
+					end
+				
+				MASTER_WAIT:
+					begin
+						if (~axs_s0_bready)
+							next_state = MASTER_WAIT;
+						else
+							next_state = AW_READY;
+					end
+
+				default: 
+					begin
+						next_state = INIT;
+					end
+			endcase
+		end
 
 endmodule
