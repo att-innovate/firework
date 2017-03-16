9437292de592fc32f85e6b6facf604039406360c
Mario Admon <mladmon589@gmail.com>
2017-03-16 16:15:53 -0700

    Implemented 'fsm_1.v' state, next state, output logic; added '// error' comments to 'fsm_0.v' for standardization

--- fsm_0.v
+++ fsm_0.v
@@ -44,281 +44,282 @@ module fsm_0 (
 		output reg  [9:0]  index
 	);
 
-		// internal registers
-		reg [3:0]  awid;
-		reg [31:0] awaddr;
-		reg [7:0]  awlen;
-		reg [2:0]  awsize;
-		reg [1:0]  awburst;
+	// internal registers
+	reg [3:0]  awid;
+	reg [31:0] awaddr;
+	reg [7:0]  awlen;
+	reg [2:0]  awsize;
+	reg [1:0]  awburst;
+
+	// state definitions (one-hot encoding)
+	parameter INIT        = 16'h0001,
+				 AW_READY    = 16'h0002,
+				 W_READY_VN  = 16'h0004,
+				 W_READY_VL  = 16'h0008,
+				 W_READY_RN  = 16'h0010,
+				 W_READY_RL  = 16'h0020,
+				 VF_FULL     = 16'h0040,
+				 RF_FULL     = 16'h0080,
+				 B_READY_VN  = 16'h0100,
+				 B_READY_VL  = 16'h0200,
+				 B_READY_RN  = 16'h0400,
+				 B_READY_RL  = 16'h0800,
+				 MASTER_WAIT = 16'h1000;
+
+	// state memory logic
+	reg [15:0] state;
+	reg [15:0] next_state;
 	
-		// state definitions (one-hot encoding)
-		parameter INIT        = 16'h0001,
-		          AW_READY    = 16'h0002,
-		          W_READY_VN  = 16'h0004,
-		          W_READY_VL  = 16'h0008,
-		          W_READY_RN  = 16'h0010,
-		          W_READY_RL  = 16'h0020,
-		          VF_FULL     = 16'h0040,
-		          RF_FULL     = 16'h0080,
-		          B_READY_VN  = 16'h0100,
-		          B_READY_VL  = 16'h0200,
-		          B_READY_RN  = 16'h0400,
-		          B_READY_RL  = 16'h0800,
-		          MASTER_WAIT = 16'h1000;
+	always @(posedge clk)
+	begin
+		if (reset)
+			state <= INIT;
+		else
+			state <= next_state;
+	end
 
-		// state memory logic
-		reg [15:0] state;
-		reg [15:0] next_state;
+	// next state, output logic
+	always @*
+	begin
+		// default values (may be overwritten)
+		axs_s0_awready = 1'b0;
+		axs_s0_wready = 1'b0;
+		axs_s0_bvalid = 1'b0;
+		axs_s0_bid = awid;
+
+		varint_in_fifo_clr = 1'b0;
+		varint_in_index_clr = 1'b0;
+		raw_data_in_fifo_clr = 1'b0;
+		raw_data_in_index_clr = 1'b0;
+		raw_data_in_wstrb_clr = 1'b0;
+
+		varint_in_fifo_push = 1'b0;
+		varint_in_index_push = 1'b0;
+		raw_data_in_fifo_push = 1'b0;
+		raw_data_in_index_push = 1'b0;
+		raw_data_in_wstrb_push = 1'b0;
 		
-		always @(posedge clk)
-		begin
-			if (reset)
-				state <= INIT;
-			else
-				state <= next_state;
-		end
+		case (state)
+			INIT:		
+				begin
+					varint_in_fifo_clr = 1'b1;
+					varint_in_index_clr = 1'b1;
+					raw_data_in_fifo_clr = 1'b1;
+					raw_data_in_index_clr = 1'b1;
+					raw_data_in_wstrb_clr = 1'b1;
 
-		// next state, output logic
-		always @*
-		begin
-			axs_s0_awready = 1'b0;
-			axs_s0_wready = 1'b0;
-			axs_s0_bvalid = 1'b0;
-			axs_s0_bid = awid;
+					awid = 4'h0;
+					awaddr = 32'h0000_0000;
+					awlen = 8'h00;
+					awsize = 3'b000;
+					awburst = 2'b00;
+					wdata = 32'h0000_0000;
+					wstrb = 4'h0;
+					index = 10'b0000000000;
 
-			varint_in_fifo_clr = 1'b0;
-			varint_in_index_clr = 1'b0;
-			raw_data_in_fifo_clr = 1'b0;
-			raw_data_in_index_clr = 1'b0;
-			raw_data_in_wstrb_clr = 1'b0;
+					next_state = AW_READY;
+				end
 
-			varint_in_fifo_push = 1'b0;
-			varint_in_index_push = 1'b0;
-			raw_data_in_fifo_push = 1'b0;
-			raw_data_in_index_push = 1'b0;
-			raw_data_in_wstrb_push = 1'b0;
-			
-			case (state)
-				INIT:		
-					begin
-						varint_in_fifo_clr = 1'b1;
-						varint_in_index_clr = 1'b1;
-						raw_data_in_fifo_clr = 1'b1;
-						raw_data_in_index_clr = 1'b1;
-						raw_data_in_wstrb_clr = 1'b1;
-
-						awid = 4'h0;
-						awaddr = 32'h0000_0000;
-						awlen = 8'h00;
-						awsize = 3'b000;
-						awburst = 2'b00;
-						wdata = 32'h0000_0000;
-						wstrb = 4'h0;
-						index = 10'b0000000000;
-
+			AW_READY:
+				begin
+					axs_s0_bvalid = 1'b0;
+					axs_s0_awready = 1'b1;
+					
+					awid = axs_s0_awid;
+					awaddr = axs_s0_awaddr;
+					awlen = axs_s0_awlen;
+					awsize = axs_s0_awsize;
+					awburst = axs_s0_awburst;
+				
+					if (~axs_s0_awvalid)
 						next_state = AW_READY;
-					end
+					else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'h0x
+									&& varint_in_fifo_full)
+						next_state = VF_FULL;
+					else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'h00 
+									&& ~varint_in_fifo_full)
+						next_state = W_READY_VN;
+					else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'h01
+									&& ~varint_in_fifo_full)
+						next_state = W_READY_VL;
+					else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'hFx
+									&& raw_data_in_fifo_full)
+						next_state = RF_FULL;
+					else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'hF0
+									&& ~raw_data_in_fifo_full)
+						next_state = W_READY_RN;
+					else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'hF1
+									&& ~raw_data_in_fifo_full)
+						next_state = W_READY_RL;
+					else // error
+						next_state = INIT;
+				end
 
-				AW_READY:
-					begin
-						axs_s0_bvalid = 1'b0;
-						axs_s0_awready = 1'b1;
-						
-						awid = axs_s0_awid;
-						awaddr = axs_s0_awaddr;
-						awlen = axs_s0_awlen;
-						awsize = axs_s0_awsize;
-						awburst = axs_s0_awburst;
+			W_READY_VN:
+				begin
+					axs_s0_awready = 1'b0;
+					axs_s0_wready = 1'b1;
 					
-						if (~axs_s0_awvalid)
-							next_state = AW_READY;
-						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'h0x
-										&& varint_in_fifo_full)
-							next_state = VF_FULL;
-						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'h00 
-										&& ~varint_in_fifo_full)
-							next_state = W_READY_VN;
-						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'h01
-										&& ~varint_in_fifo_full)
-							next_state = W_READY_VL;
-						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'hFx
-										&& raw_data_in_fifo_full)
-							next_state = RF_FULL;
-						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'hF0
-										&& ~raw_data_in_fifo_full)
-							next_state = W_READY_RN;
-						else if (axs_s0_awvalid && axs_s0_awaddr[7:0] == 8'hF1
-										&& ~raw_data_in_fifo_full)
-							next_state = W_READY_RL;
-						else 
-							next_state = INIT;
-					end
+					wdata = axs_s0_wdata;
+					wstrb = axs_s0_wstrb;
+				
+					if (~axs_s0_wvalid)
+						next_state = W_READY_VN;
+					else
+						next_state = B_READY_VN;
+				end
+			
+			W_READY_VL:
+				begin
+					axs_s0_awready = 1'b0;
+					axs_s0_wready = 1'b1;
+					
+					wdata = axs_s0_wdata;
+					wstrb = axs_s0_wstrb;
+				
+					if (~axs_s0_wvalid)
+						next_state = W_READY_VL;
+					else
+						next_state = B_READY_VL;
+				end
+			
+			W_READY_RN:
+				begin
+					axs_s0_awready = 1'b0;
+					axs_s0_wready = 1'b1;
+					
+					wdata = axs_s0_wdata;
+					wstrb = axs_s0_wstrb;
+				
+					if (~axs_s0_wvalid)
+						next_state = W_READY_RN;
+					else
+						next_state = B_READY_RN;
+				end
+			
+			W_READY_RL:
+				begin
+					axs_s0_awready = 1'b0;
+					axs_s0_wready = 1'b1;
+					
+					wdata = axs_s0_wdata;
+					wstrb = axs_s0_wstrb;
+				
+					if (~axs_s0_wvalid)
+						next_state = W_READY_RL;
+					else
+						next_state = B_READY_RL;
+				end
+			
+			VF_FULL:
+				begin
+					axs_s0_awready = 1'b0;
+					axs_s0_wready = 1'b0;
+				
+					if (varint_in_fifo_full)
+						next_state = VF_FULL;
+					else if (~varint_in_fifo_full && awaddr[7:0] == 8'h00)
+						next_state = W_READY_VN;
+					else if (~varint_in_fifo_full && awaddr[7:0] == 8'h01)
+						next_state = W_READY_VL;
+					else // error
+						next_state = INIT;
+				end
+			
+			RF_FULL:
+				begin
+					axs_s0_awready = 1'b0;
+					axs_s0_wready = 1'b0;
+				
+					if (raw_data_in_fifo_full)
+						next_state = RF_FULL;
+					else if (~raw_data_in_fifo_full && awaddr[7:0] == 8'hF0)
+						next_state = W_READY_RN;
+					else if (~raw_data_in_fifo_full && awaddr[7:0] == 8'hF1)
+						next_state = W_READY_RL;
+					else // error
+						next_state = INIT;
+				end
+			
+			B_READY_VN:
+				begin
+					axs_s0_wready = 1'b0;
+					axs_s0_bvalid = 1'b1;
+					axs_s0_bid = awid;
 
-				W_READY_VN:
-					begin
-						axs_s0_awready = 1'b0;
-						axs_s0_wready = 1'b1;
-						
-						wdata = axs_s0_wdata;
-						wstrb = axs_s0_wstrb;
-					
-						if (~axs_s0_wvalid)
-							next_state = W_READY_VN;
-						else
-							next_state = B_READY_VN;
-					end
+					varint_in_fifo_push = 1'b1;
+					varint_in_index_push = 1'b1;
 				
-				W_READY_VL:
-					begin
-						axs_s0_awready = 1'b0;
-						axs_s0_wready = 1'b1;
-						
-						wdata = axs_s0_wdata;
-						wstrb = axs_s0_wstrb;
-					
-						if (~axs_s0_wvalid)
-							next_state = W_READY_VL;
-						else
-							next_state = B_READY_VL;
-					end
-				
-				W_READY_RN:
-					begin
-						axs_s0_awready = 1'b0;
-						axs_s0_wready = 1'b1;
-						
-						wdata = axs_s0_wdata;
-						wstrb = axs_s0_wstrb;
-					
-						if (~axs_s0_wvalid)
-							next_state = W_READY_RN;
-						else
-							next_state = B_READY_RN;
-					end
-				
-				W_READY_RL:
-					begin
-						axs_s0_awready = 1'b0;
-						axs_s0_wready = 1'b1;
-						
-						wdata = axs_s0_wdata;
-						wstrb = axs_s0_wstrb;
-					
-						if (~axs_s0_wvalid)
-							next_state = W_READY_RL;
-						else
-							next_state = B_READY_RL;
-					end
-				
-				VF_FULL:
-					begin
-						axs_s0_awready = 1'b0;
-						axs_s0_wready = 1'b0;
-					
-						if (varint_in_fifo_full)
-							next_state = VF_FULL;
-						else if (~varint_in_fifo_full && awaddr[7:0] == 8'h00)
-							next_state = W_READY_VN;
-						else if (~varint_in_fifo_full && awaddr[7:0] == 8'h01)
-							next_state = W_READY_VL;
-						else
-							next_state = INIT;
-					end
-				
-				RF_FULL:
-					begin
-						axs_s0_awready = 1'b0;
-						axs_s0_wready = 1'b0;
-					
-						if (raw_data_in_fifo_full)
-							next_state = RF_FULL;
-						else if (~raw_data_in_fifo_full && awaddr[7:0] == 8'hF0)
-							next_state = W_READY_RN;
-						else if (~raw_data_in_fifo_full && awaddr[7:0] == 8'hF1)
-							next_state = W_READY_RL;
-						else
-							next_state = INIT;
-					end
-				
-				B_READY_VN:
-					begin
-						axs_s0_wready = 1'b0;
-						axs_s0_bvalid = 1'b1;
-						axs_s0_bid = awid;
+					if (~axs_s0_bready)
+						next_state = MASTER_WAIT;
+					else
+						next_state = AW_READY;
+				end
+			
+			B_READY_VL:
+				begin
+					axs_s0_wready = 1'b0;
+					axs_s0_bvalid = 1'b1;
+					axs_s0_bid = awid;
 
-						varint_in_fifo_push = 1'b1;
-						varint_in_index_push = 1'b1;
+					varint_in_fifo_push = 1'b1;
+					varint_in_index_push = 1'b1;
 					
-						if (~axs_s0_bready)
-							next_state = MASTER_WAIT;
-						else
-							next_state = AW_READY;
-					end
+					index = (index == 1023) ? 0 : index + 1;
 				
-				B_READY_VL:
-					begin
-						axs_s0_wready = 1'b0;
-						axs_s0_bvalid = 1'b1;
-						axs_s0_bid = awid;
+					if (~axs_s0_bready)
+						next_state = MASTER_WAIT;
+					else
+						next_state = AW_READY;
+				end
 
-						varint_in_fifo_push = 1'b1;
-						varint_in_index_push = 1'b1;
-						
-						index = (index == 1023) ? 0 : index + 1;
-					
-						if (~axs_s0_bready)
-							next_state = MASTER_WAIT;
-						else
-							next_state = AW_READY;
-					end
+			B_READY_RN:
+				begin
+					axs_s0_wready = 1'b0;
+					axs_s0_bvalid = 1'b1;
+					axs_s0_bid = awid;
 
-				B_READY_RN:
-					begin
-						axs_s0_wready = 1'b0;
-						axs_s0_bvalid = 1'b1;
-						axs_s0_bid = awid;
-
-						raw_data_in_fifo_push = 1'b1;
-						raw_data_in_index_push = 1'b1;
-						raw_data_in_wstrb_push = 1'b1;
-					
-						if (~axs_s0_bready)
-							next_state = MASTER_WAIT;
-						else
-							next_state = AW_READY;
-					end
+					raw_data_in_fifo_push = 1'b1;
+					raw_data_in_index_push = 1'b1;
+					raw_data_in_wstrb_push = 1'b1;
 				
-				B_READY_RL:
-					begin
-						axs_s0_wready = 1'b0;
-						axs_s0_bvalid = 1'b1;
-						axs_s0_bid = awid;
+					if (~axs_s0_bready)
+						next_state = MASTER_WAIT;
+					else
+						next_state = AW_READY;
+				end
+			
+			B_READY_RL:
+				begin
+					axs_s0_wready = 1'b0;
+					axs_s0_bvalid = 1'b1;
+					axs_s0_bid = awid;
 
-						raw_data_in_fifo_push = 1'b1;
-						raw_data_in_index_push = 1'b1;
-						raw_data_in_wstrb_push = 1'b1;
-						
-						index = (index == 1023) ? 0 : index + 1;
+					raw_data_in_fifo_push = 1'b1;
+					raw_data_in_index_push = 1'b1;
+					raw_data_in_wstrb_push = 1'b1;
 					
-						if (~axs_s0_bready)
-							next_state = MASTER_WAIT;
-						else
-							next_state = AW_READY;
-					end
+					index = (index == 1023) ? 0 : index + 1;
 				
-				MASTER_WAIT:
-					begin
-						axs_s0_bvalid = 1'b1;
-						axs_s0_bid = awid;
-					
-						if (~axs_s0_bready)
-							next_state = MASTER_WAIT;
-						else
-							next_state = AW_READY;
-					end
+					if (~axs_s0_bready)
+						next_state = MASTER_WAIT;
+					else
+						next_state = AW_READY;
+				end
+			
+			MASTER_WAIT:
+				begin
+					axs_s0_bvalid = 1'b1;
+					axs_s0_bid = awid;
+				
+					if (~axs_s0_bready)
+						next_state = MASTER_WAIT;
+					else
+						next_state = AW_READY;
+				end
 
-				default:
-					next_state = INIT;
-			endcase
-		end
+			default:
+				next_state = INIT;
+		endcase
+	end
 endmodule
--- fsm_1.v
+++ fsm_1.v
@@ -18,6 +18,117 @@ module fsm_1 (
 		output reg  [1:0] raw_data_sel
 	);
 
-	
+	// internal registers
+	reg [1:0] index;
 
+	// state definitions (one-hot encoding)
+	parameter INIT      = 8'h01,
+	          RD_READY  = 8'h02,
+	          RF_FULL   = 8'h04,
+	          ENCODE_0  = 8'h08,
+	          ENCODE_1  = 8'h10,
+	          ENCODE_2  = 8'h20,
+	          ENCODE_3  = 8'h40;
+
+	// state memory logic
+	reg [7:0] state;
+	reg [7:0] next_state;
+	
+	always @(posedge clk)
+	begin
+		if (reset)
+			state <= INIT;
+		else
+			state <= next_state;
+	end
+	
+	// next state, output logic
+	always @*
+	begin
+		// default values (may be overwritten)
+		raw_data_in_fifo_pop = 1'b0;
+		raw_data_in_index_pop = 1'b0;
+		raw_data_in_wstrb_pop = 1'b0;
+
+		raw_data_out_fifo_clr = 1'b0;
+		raw_data_out_index_clr = 1'b0;
+		
+		raw_data_sel = index;
+
+		case (state)
+			INIT:
+				begin
+					raw_data_out_fifo_clr = 1'b1;
+					raw_data_out_index_clr = 1'b1;
+					index = 2'b00;
+
+					next_state = RD_READY;
+				end
+
+			RD_READY:
+				begin
+					raw_data_in_fifo_pop = 1'b1;
+					raw_data_in_index_pop = 1'b1;
+					raw_data_in_wstrb_pop = 1'b1;
+					
+					if (raw_data_in_fifo_empty)
+						next_state = RD_READY;
+					else if (~raw_data_in_fifo_empty && raw_data_out_fifo_full)
+						next_state = RF_FULL;
+					else
+						next_state = ENCODE_0;
+				end
+			
+			RF_FULL:
+				begin
+					if (raw_data_out_fifo_full)
+						next_state = RF_FULL;
+					else if (~raw_data_out_fifo_full && index == 2'b00)
+						next_state = ENCODE_0;
+					else if (~raw_data_out_fifo_full && index == 2'b01)
+						next_state = ENCODE_1;
+					else if (~raw_data_out_fifo_full && index == 2'b10)
+						next_state = ENCODE_2;
+					else if (~raw_data_out_fifo_full && index == 2'b11)
+						next_state = ENCODE_3;
+					else // error
+						next_state = INIT;
+				end
+
+			ENCODE_0:
+				begin
+					// raw_data_sel == 2'b00
+					index = index + 1;
+					
+					next_state = RF_FULL;
+				end
+
+			ENCODE_1:
+				begin
+					// raw_data_sel == 2'b01
+					index = index + 1;
+					
+					next_state = RF_FULL;
+				end
+
+			ENCODE_2:
+				begin
+					// raw_data_sel == 2'b10
+					index = index + 1;
+					
+					next_state = RF_FULL;
+				end
+
+			ENCODE_3:
+				begin 
+					// raw_data_sel == 2'b11
+					index = 2'b00;
+					
+					next_state = RD_READY;
+				end
+
+			default:
+				next_state = INIT;
+		endcase
+	end
 endmodule
