cb65abdab84f4d178fd71c67d7930e6e271c61a5
Mario Admon <mladmon589@gmail.com>
2017-03-16 17:06:05 -0700

    Created 'fsm_2.v'; implemented module, port declarations; happy birthday Erik

---
+++ fsm_2.v
@@ -0,0 +1,22 @@
+// fsm_2.v
+
+module fsm_2 (
+		// global signals
+		input  wire clk,
+		input  wire reset,
+		
+		// datapath control signals
+		input  wire varint_in_fifo_empty,
+		output reg  varint_in_fifo_pop,
+		output reg  varint_in_index_pop,
+		
+		input  wire varint_out_fifo_full,
+		output reg  varint_out_fifo_clr,
+		output reg  varint_out_fifo_push,
+		output reg  varint_out_index_clr,
+		output reg  varint_out_index_push,
+		
+		// TODO: figure out where to build the datapath
+	);
+
+endmodule
