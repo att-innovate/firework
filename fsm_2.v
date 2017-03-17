ac65cf0e0ef3e65dfed3c53c282709b6e5644f18
Mario Admon <mladmon589@gmail.com>
2017-03-17 11:34:32 -0700

    Implemented state definitions, state memory logic

--- fsm_2.v
+++ fsm_2.v
@@ -17,8 +17,32 @@ module fsm_2 (
 		output reg        varint_out_index_push,
 		
 		// datapath data signals
-		input wire [31:0] varint_in,
-		output reg [7:0]  varint_out
+		input wire [31:0] varint_data_in,
+		output reg [7:0]  varint_data_out
 	);
+	
+	// internal wires, registers
+	reg varint_in_sel, check_cond_sel, varint_out_sel;
+	reg [31:0] varint_data;
+	
+	// state definitions (one-hot encoding)
+	parameter INIT       = 8'h01,
+	          V_READY    = 8'h02,
+	          LOAD_COND  = 8'h04,
+	          VF_FULL    = 8'h08,
+	          ENCODE_N   = 8'h10,
+	          ENCODE_L   = 8'h20;
+
+	// state memory logic
+	reg [7:0] state;
+	reg [7:0] next_state;
+	
+	always @(posedge clk)
+	begin
+		if (reset)
+			state <= INIT;
+		else
+			state <= next_state;
+	end
 
 endmodule
