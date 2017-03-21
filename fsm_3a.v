98267150200e72bdfb484979f1ed4cea6057ad97
Mario Admon <mladmon589@gmail.com>
2017-03-21 10:19:48 -0700

    Implemented state memory, next state, and output logic

--- fsm_3a.v
+++ fsm_3a.v
@@ -14,5 +14,59 @@ module fsm_3a (
 		input  wire  varint_data_accepted,
 		output wire  varint_data_valid
 	);
+
+	// state definitions (one-hot encoding)
+	parameter INIT    = 3'b001,
+	          V_FETCH = 3'b010,
+				 V_READY = 3'b100;
+				 
+	// state memory logic
+	reg [2:0] state;
+	reg [2:0] next_state;
 	
+	always @(posedge clk)
+	begin
+		if (reset)
+			state <= INIT;
+		else
+			state <= next_state;
+	end
+
+	// next state, output logic
+	always @*
+	begin
+		// default values (may be overwritten)
+		varint_out_fifo_pop = 1'b0;
+		varint_out_index_pop = 1'b0;
+		varint_data_valid = 1'b0;
+		
+		case (state)
+			INIT:
+				next_state = V_FETCH;
+
+			V_FETCH:
+				begin
+					varint_out_fifo_pop = 1'b1;
+					varint_out_index_pop = 1'b1;
+					
+					if (varint_out_fifo_empty)
+						next_state = V_FETCH;
+					else
+						next_state = V_READY;
+				end
+
+			V_READY:
+				begin
+					varint_data_valid = 1'b1;
+					
+					if (varint_data_accepted)
+						next_state = V_FETCH;
+					else
+						next_state = V_READY;
+				end
+
+			default:
+				next_state = INIT;
+		endcase
+	end
 endmodule
