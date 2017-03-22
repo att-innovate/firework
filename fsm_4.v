a04930226808ba42a47951fad92d804e19920c96
Mario Admon <mladmon589@gmail.com>
2017-03-21 20:28:57 -0700

    Added 'arlen' register load and data muxes

--- fsm_4.v
+++ fsm_4.v
@@ -35,14 +35,19 @@ module fsm_4 (
 	// datapath control signals
 	reg arid_clr, araddr_clr, arlen_clr, arsize_clr, arburst_clr;
 	reg arid_ld, araddr_ld, arlen_ld, arsize_ld, arburst_ld;
+	reg arlen_ld_sel, arlen_data_sel;
+	reg arlen_ld_mux;
+	
+	// datapath data signals
+	reg [7:0] arlen_data_mux;
 	
 	// state definitions (one-hot encoding)
-	parameter INIT        = 8'h01,
-	          AR_READY    = 8'h02,
-	          OF_EMPTY    = 8'h04,
-	          R_VALID_N   = 8'h08,
-	          R_VALID_L   = 8'h10,
-	          MASTER_WAIT = 8'h20;
+	parameter INIT         = 8'h01,
+	          AR_READY     = 8'h02,
+	          OF_EMPTY     = 8'h04,
+	          R_VALID_LAST = 8'h08,
+	          MASTER_WAIT  = 8'h10,
+	          R_VALID      = 8'h20;
 
 	// state memory logic
 	reg [7:0] state;
@@ -56,12 +61,114 @@ module fsm_4 (
 			arid    <= (arid_ld) ? axs_s0_arid : ((arid_clr) ? 4'h0 : arid);
 			araddr  <= (araddr_ld) ? axs_s0_araddr : 
 			           ((araddr_clr) ? 32'h0000_0000 : araddr);
-			arlen   <= (arlen_ld) ? axs_s0_arlen : ((arlen_clr) ? 8'h00 : arlen);
+			arlen   <= (arlen_ld_mux) ? arlen_data_mux : ((arlen_clr) ? 8'h00 : arlen);
 			arsize  <= (arsize_ld) ? axs_s0_arsize : ((arsize_clr) ? 3'b000 : arsize);
 			arburst <= (arburst_ld) ? axs_s0_arburst : ((arburst_clr) ? 2'b00 : arburst);
 
 			state <= next_state;
 		end
 	end
+	
+	always @*
+	begin
+		// default values (may be overwritten)
+		axs_s0_arready = 1'b0;
+		axs_s0_rlast = 1'b0;
+		axs_s0_rvalid = 1'b0;
+		
+		out_fifo_pop = 1'b0;
+		out_fifo_pop_sel = 2'b00;
+		
+		arid_clr = 1'b0;
+		arid_ld = 1'b0;
+		araddr_clr = 1'b0;
+		araddr_ld = 1'b0;
+		arlen_clr = 1'b0;
+		arlen_ld = 1'b0;
+		arsize_clr = 1'b0;
+		arsize_ld = 1'b0;
+		arburst_clr = 1'b0;
+		arburst_ld = 1'b0;
+		
+		arlen_ld_sel = 1'b0;
+		arlen_data_sel = 1'b0;
 
+		// datapath logic
+		axs_s0_rid = arid;
+
+		case (state)
+			INIT:
+				begin
+					arid_clr = 1'b1;
+					araddr_clr = 1'b1;
+					arlen_clr = 1'b1;
+					arsize_clr = 1'b1;
+					arburst_clr = 1'b1;
+
+					next_state = AR_READY;
+				end
+
+			AR_READY:
+				begin
+					axs_s0_rlast = 1'b0;
+					axs_s0_rvalid = 1'b0;
+					axs_s0_arready = 1'b1;
+
+					arid_ld = 1'b1;
+					araddr_ld = 1'b1;
+					arlen_ld = 1'b1;
+					arsize_ld = 1'b1;
+					arburst_ld = 1'b1;
+					
+					out_fifo_pop_sel = 2'b01;
+					
+					if (~axs_s0_arvalid)
+						next_state = AR_READY;
+					else if (axs_s0_arvalid && out_fifo_empty)
+						next_state = OF_EMPTY;
+					else if (axs_s0_arvalid && ~out_fifo_empty && axs_s0_arlen == 0)
+						next_state = R_VALID_LAST;
+					else if (axs_s0_arvalid && ~out_fifo_empty && 
+					            axs_s0_arlen != 0 && ~axs_s0_rready)
+						next_state = MASTER_WAIT;
+					else if (axs_s0_arvalid && ~out_fifo_empty && 
+					            axs_s0_arlen != 0 && axs_s0_rready)
+						next_state = R_VALID;
+					else // error
+						next_state = INIT;
+				end
+
+			OF_EMPTY:
+				begin
+					axs_s0_arready = 1'b0;
+					axs_s0_rvalid = 1'b0;
+					out_fifo_pop_sel = 2'b00;
+					// TODO: own this shit
+				end
+
+			R_VALID_LAST:
+				begin
+					
+				end
+
+			MASTER_WAIT:
+				begin
+					
+				end
+
+			R_VALID:
+				begin
+					
+				end
+
+			default:
+				next_state = INIT;
+		endcase
+
+		// datapath logic
+		arlen_ld_mux = (arlen_ld_sel) ? axs_s0_rready : arlen_ld;
+		
+		arlen_data_mux = (arlen_data_sel) ? arlen - 1 : axs_s0_arlen;
+		
+	end
 endmodule
