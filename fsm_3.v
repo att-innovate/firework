7dbb3a9c440178e86b56c340e70c6e1b34f12cbf
Mario Admon <mladmon589@gmail.com>
2017-03-24 14:32:42 -0700

    Fixed incorrect use of addition (+) instead of logical OR (||) operator and added parenthesis for correct operator precedence

--- fsm_3.v
+++ fsm_3.v
@@ -100,10 +100,10 @@ module fsm_3 (
 						next_state = V_PUSH_INC;
 					else if (~out_fifo_full && raw_data_valid && raw_data_eq_next)
 						next_state = R_PUSH_INC;
-					else if (out_fifo_full && (varint_data_valid && varint_eq_index +
-					                           varint_data_valid && varint_eq_next  +
-					                           raw_data_valid && raw_data_eq_index  +
-					                           raw_data_valid && raw_data_eq_next))
+					else if (out_fifo_full && ((varint_data_valid && varint_eq_index) ||
+					                           (raw_data_valid && raw_data_eq_index)  ||
+					                           (varint_data_valid && varint_eq_next)  ||
+					                           (raw_data_valid && raw_data_eq_next)))
 						next_state = OF_FULL;
 					else
 						next_state = WAIT_DATA;
